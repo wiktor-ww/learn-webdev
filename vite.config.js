b4ef33a93b519b7774a063945df6a2ae6fc37749
Wiktor <wiktor.ww@hotmail.com>
2026-03-25 18:38:10 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/learn-webdev/',
   plugins: [vue()],
+  base: '/learn-webdev/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // to wyciszy ewentualne ostrzeżenia Sassa
+      }
+    }
+  }
 })
